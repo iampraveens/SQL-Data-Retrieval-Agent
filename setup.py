b33bb80a2d6948b25956ab41a528a77d8ba5e941
iampraveens <praveensivaprakasham@gmail.com>
2025-08-26 02:46:08 +0530

    setup.py implemented and requirements,txt updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="SQL-Data-Retrieval-Agent",
+    version="0.1.0",
+    packages=find_packages(),
+    author="iampraveens",
+    author_email="praveensivaprakasham@gmail.com",
+    description="An agent for retrieving data from SQL databases",
+    install_requires=[]
+)
